465d01e29f87f723712c197c240640a75fc41a82
hohongdu <pke0309@naver.com>
2024-10-23 10:27:05 +0900

    10_23

---
+++ admin/src/main/java/edu/sm/app/dto/Search.java
@@ -0,0 +1,13 @@
+package edu.sm.app.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Search {
+    private String keyword;
+    private String search;
+}
